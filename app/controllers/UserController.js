9c7ac11842062d2494bb0abe7bdab82c4dbd491b
lopesjpaulo <lopes_jpaulo@hotmail.com>
2019-09-10 11:22:08 -0300

    Função de login

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -57,6 +57,27 @@ class UserController{
         }    
     }
 
+    static async login(req, res) {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
+
+        try {
+            const user = await models.User.findOne({
+                where: {
+                    email: req.body.email,
+                    password: req.body.password
+                }
+            });
+
+            if(!user) return res.status(204).json();
+
+            return res.status(200).json(user);
+        } catch (error) {
+            return res.status(500).json({error});
+        }
+    }
+
     static async update(req, res) {
         try {
             if(!req.params.id) return res.status(400).json();
@@ -75,19 +96,17 @@ class UserController{
     }
 
     static async updateInstruments(req, res) {
-        try {
+        
             if(!req.params.id) return res.status(400).json();
 
             const instruments = req.body.instruments;
 
-            instruments.forEach(Instrument => {
-                user.setIntruments(parseInt(Instrument));
+            instruments.forEach(instrument => {
+                models.User.setIntruments(parseInt(instrument));
             });
 
             return res.status(200).json();
-        } catch (error) {
-            return res.status(500).json({error});
-        }
+        
     }
 
     static async destroy(req, res) {
