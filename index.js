3e4fac793ceb34b900b873380fae658aec406e3c
lopesjpaulo <lopes_jpaulo@hotmail.com>
2019-10-09 10:30:39 -0300

    Enabled cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./app/routes/routes');
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.urlencoded({
     extended: true
