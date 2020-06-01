8457e2299f77a097676f5b070a9ce5893fe3f2b1
lopesjpaulo <lopes_jpaulo@hotmail.com>
2020-06-01 12:12:55 -0300

    Ajustes na validação

--- app/helpers/validate.js
+++ app/helpers/validate.js
@@ -243,23 +243,3 @@ module.exports = method => {
         }
     }
 };
-
-/*class Validate{
-    constructor(method){
-        this.method = method;
-    }
-
-    static validInput(){
-        switch(this.method){
-            case 'create-user': {
-                return [
-                    check('name').exists().not().isEmpty(),
-                    check('email').exists().isEmail(),
-                    check('password').exists().not().isEmpty()
-                ]
-            }
-        }
-    }
-}
-
-module.exports = Validate*/
--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-//User.create({ name: 'João', email: 'joao@maxmeio.com', password: '123456'});
 app.use('/', routes);
 
 app.listen(PORT);
