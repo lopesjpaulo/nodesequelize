556b3f66ba93fbc088db2d0cc31472cf1abd459c
lopesjpaulo <lopes_jpaulo@hotmail.com>
2020-07-30 12:32:03 -0300

    Ajustes na rota

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -46,6 +46,7 @@ routes.get("/teachers", verifyJWT, TeacherController.index);
 routes.get("/teachers/:id", verifyJWT, TeacherController.show);
 routes.post("/teachers", [verifyJWT, validate("teacher")], TeacherController.store);
 routes.put("/teachers/:id", verifyJWT, TeacherController.update);
+routes.delete("/teachers/:id", verifyJWT, TeacherController.destroy);
 routes.put("/teachers", verifyJWT, TeacherController.updateLogged);
 routes.put("/teachers/instruments", verifyJWT, TeacherController.updateInstruments);
 routes.get("/teachers/classes/:id", verifyJWT, TeacherController.countClasses);
