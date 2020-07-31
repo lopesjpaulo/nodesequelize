d32d75d3ae48dc9e5bc209779cc2bb4d2939672d
robsonsllva2 <robsonsilva@rits.com.br>
2020-07-31 19:59:32 -0300

    not expires token

--- app/middlewares/verifyJWT.js
+++ app/middlewares/verifyJWT.js
@@ -4,7 +4,7 @@ function verifyJWT(req, res, next){
   let token = req.headers['authorization'];
   if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
 
-  jwt.verify(token, process.env.SECRET, function(err, decoded) {
+  jwt.verify(token, process.env.SECRET,{ ignoreExpiration: true }, function(err, decoded) {
       if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
 
       req.userId = decoded.id;
